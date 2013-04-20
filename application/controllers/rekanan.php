09feae6622285cdf9f184123c773afa89a665ef9
duniadua <duniadua@gmail.com>
2013-04-20 23:35:15 +0700

    add new class rekanan

---
+++ application/controllers/rekanan.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of rekanan
+ *
+ * @author sahid
+ */
+class rekanan extends CI_Controller {
+
+    //put your code here
+    public function Rekanan() {
+        parent::__construct();
+    }
+
+    public function add_catagory() {
+        
+    }
+
+    public function add_rekan() {
+        
+    }
+
+    public function del_catagory() {
+        
+    }
+
+    public function del_rekan() {
+        
+    }
+
+    public function edit_rekan() {
+        
+    }
+
+}
+
+?>
