f2e9528d758f220b25f75cb283af7362b8b9b32e
duniadua <duniadua@gmail.com>
2013-04-20 23:41:02 +0700

    Create Model untuk rekanan

---
+++ application/models/rekanan_model.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of rekanan_model
+ *
+ * @author sahid
+ */
+class rekanan_model extends CI_Model{
+    //put your code here
+}
+
+?>
