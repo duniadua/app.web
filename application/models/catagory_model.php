4a671162a9bdca23011a790fc303759943e3c6c4
duniadua <duniadua@gmail.com>
2013-04-23 22:38:42 +0700

    catagory model

---
+++ application/models/catagory_model.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of catagory_model
+ *
+ * @author sahid
+ */
+class catagory_model extends CI_Model {
+    //put your code here
+    public function Catagory_model(){
+        parent::__construct();
+        }
+        
+}
+
+?>
